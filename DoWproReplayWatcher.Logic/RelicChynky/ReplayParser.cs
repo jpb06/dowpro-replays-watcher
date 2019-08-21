1ab8bebd244aff618c6a890516ddbe7298211972
jpb <jpb.06@outlook.com>
2019-08-21 20:04:23 +0200

    Amending Relic Chunky parser to allow replay name alteration

--- DoWproReplayWatcher.Logic/RelicChynky/ReplayParser.cs
+++ DoWproReplayWatcher.Logic/RelicChynky/ReplayParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoWproReplayWatcher.Logic.Types;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,27 +10,88 @@ namespace DoWproReplayWatcher.RelicChunky
 {
     public static class ReplayParser
     {
-        public static string GetMapName(string filePath)
+        public static RelicChunkyData Fetch(string filePath)
         {
+            string mapName = string.Empty;
+            string modName = string.Empty;
+            string replayName = string.Empty;
+            long replayNameLengthPosition = 0;
+            long replayNameValueEndPosition = 0;
+            long databaseChunkLengthPosition = 0;
+            long headerLengthPosition = 0;
+            int databaseChunkLength = 0;
+            int headerLength = 0;
+
             using (FileStream stream = File.Open(filePath, FileMode.Open))
             {
                 using (BinaryReader reader = new BinaryReader(stream, Encoding.ASCII))
                 {
-                    stream.Position = 234;
-                    //[DWORD]->%length% // length of additional header infos
+                    //[DWORD]->%length% // length of mod name
                     int length = reader.ReadInt32();
                     //[UNICODETEXT] // Double-byte engine version string (%length%*2)
-                    byte[] buffer = reader.ReadBytes(length * 2);
-                    //string engine = new UnicodeEncoding().GetString(buffer);
-                    //[DWORD]->%length% // lenth of map Name
-                    length = reader.ReadInt32();
-                    //[TEXT] // Map name (%length%)
                     char[] chr = reader.ReadChars(length);
+                    modName = new string(chr).TrimEnd(new char[] { '\0' });
 
-                    string fullName = new string(chr);
+                    headerLengthPosition = stream.Position = 153;
+                    //[DWORD]->%length% // length of header
+                    headerLength = reader.ReadInt32();
 
-                    return fullName.Substring(fullName.LastIndexOf("\\")+1);
+                    stream.Position = 234;
+                    //[DWORD]->%length% // length of map name
+                    length = reader.ReadInt32();
+                    //[UNICODETEXT] // Double-byte map name string (%length%*2)
+                    byte[] buffer = reader.ReadBytes(length * 2);
+
+                    //[DWORD]->%length% // length of map internal name
+                    length = reader.ReadInt32();
+                    //[TEXT] // Map internal name (%length%)
+                    chr = reader.ReadChars(length);
+
+                    mapName = new string(chr);
+                    mapName = mapName.Substring(mapName.LastIndexOf("\\") + 1);
+
+                    reader.BaseStream.Position += 16;
+
+                    reader.BaseStream.Position += 8; // DATABASE
+                    reader.BaseStream.Position += 4; // ?
+
+                    databaseChunkLengthPosition = reader.BaseStream.Position;
+                    //[DWORD]->%length% // length of DATABASE chunk
+                    databaseChunkLength = reader.ReadInt32();
+                    reader.BaseStream.Position += 24;
+
+                    reader.BaseStream.Position += 61; // game options
+
+                    replayNameLengthPosition = reader.BaseStream.Position;
+
+                    //[DWORD]->%length% // length of replay name
+                    length = reader.ReadInt32();
+                    //[UNICODETEXT] // Double-byte replay name string (%length%*2)
+                    buffer = reader.ReadBytes(length * 2);
+                    replayName = new UnicodeEncoding().GetString(buffer);
+
+                    replayNameValueEndPosition = reader.BaseStream.Position;
                 }
+
+                if (string.IsNullOrEmpty(modName) || 
+                    string.IsNullOrEmpty(mapName) ||
+                    string.IsNullOrEmpty(replayName)) return null;
+
+                return new RelicChunkyData
+                {
+                    ModName = modName,
+                    MapName = mapName,
+                    ReplayName = replayName,
+
+                    ReplayNameLengthPosition = replayNameLengthPosition,
+                    ReplayNameValueEndPosition = replayNameValueEndPosition,
+                    DatabaseChunkLengthPosition = databaseChunkLengthPosition,
+                    HeaderLengthPosition = headerLengthPosition,
+
+                    DatabaseChunkLength = databaseChunkLength,
+                    HeaderLength = headerLength
+                };
+
             }
         }
     }
