33c35537965b752fdaabd7bcd57659af51ab0be4
jpb <jpb.06@outlook.com>
2019-08-21 20:05:10 +0200

    Relic Chunky parsing result type

---
+++ DoWproReplayWatcher.Logic/Types/RelicChunkyData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWproReplayWatcher.Logic.Types
+{
+    public class RelicChunkyData
+    {
+        public string ModName { get; set; }
+        public string MapName { get; set; }
+        public string ReplayName { get; set; }
+
+        public long DatabaseChunkLengthPosition { get; set; }
+        public long ReplayNameLengthPosition { get; set; }
+        public long ReplayNameValueEndPosition { get; set; }
+        public long HeaderLengthPosition { get; set; }
+
+        public int DatabaseChunkLength { get; set; }
+        public int HeaderLength { get; set; }
+    }
+}
